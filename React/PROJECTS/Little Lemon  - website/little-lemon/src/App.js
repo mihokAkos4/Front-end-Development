febc4473e522ace4fd553d8f5304760b8585d420
mihokAkos4 <mihok.akos4@gmail.com>
2025-01-25 20:41:24 +0200

    REACT

--- React/PROJECTS/Little Lemon  - website/little-lemon/src/App.js
+++ React/PROJECTS/Little Lemon  - website/little-lemon/src/App.js
@@ -1,15 +1,59 @@
-import React, { Component } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { auth, onAuthStateChanged } from './firebase';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>Homepage</p>
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+
+import AboutPage from './pages/AboutPage';
+import ReservationsPage from './pages/ReservationsPage';
+import MenuPage from './pages/MenuPage';
+import OrderOnlinePage from './pages/OrderOnlinePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+
+
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    document.title = "Little Lemon"; // Set the new title here
+  }, []);
+
+  useEffect(() => {
+    // Track authentication state
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user); // User is signed in
+      } else {
+        setUser(null); // User is signed out
+      }
+    });
+
+    return () => unsubscribe(); // Cleanup on unmount
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/reservations" element={<ReservationsPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/order" element={<OrderOnlinePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- React/PROJECTS/Little Lemon  - website/little-lemon/src/index.js
+++ React/PROJECTS/Little Lemon  - website/little-lemon/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+// Create a root element
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Render your app
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
